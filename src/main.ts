441bed08986a6f39d6e3ffa661d81e074499018b
Alan Flores <alanif@hotmail.com>
2025-04-06 11:59:13 -0500

    feat; adding cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
